bff5f3acc008cfec535af880c08000cd3428cbbb
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-11-27 19:25:44 -0300

    Add .env.production to .gitignore and set publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  
+  publicPath: process.env.VUE_APP_BASE_URL || '/',
   // Configuración del servidor de desarrollo
   devServer: {
     host: 'localhost', // Cambia 'localhost' a la IP deseada si es necesario
