d2b4bd447ae1165fa5e031d782167e78320ea4c1
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-11-27 10:05:13 -0300

    Add documentation comment to App.vue for clarity on component purpose

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<!--
+Path: src/App.vue
+Este es el componente principal de la aplicación. Aquí se importa el componente MadyBot_Vue y se muestra en la plantilla.
+-->
+
 <template>
   <div id="app">
     <MadyBot_Vue />
