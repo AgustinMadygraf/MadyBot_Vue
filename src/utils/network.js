f07ca05be076feeec865428281b2efce6f07e18d
AgustinMadygraf <agustin.mtto.madygraf>
2024-11-27 21:45:31 -0300

    Update backend connection check to use POST method with JSON payload

--- src/utils/network.js
+++ src/utils/network.js
@@ -6,9 +6,25 @@ export async function checkBackendConnection() {
       const url = 'http://192.168.0.125:5000/receive-data';
       console.log('URL del backend:', url);
       const response = await fetch(url, {
-        method: 'POST', // Cambiado de 'GET' a 'POST'
-        mode: 'cors'
-      });
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json' // Add this line
+        },
+        body: JSON.stringify({
+            prompt_user: "Hello, MadyBotPy!",
+            user_data: {
+              id: "12345",
+              browserData: {
+                userAgent: navigator.userAgent,
+                screenResolution: `${window.screen.width}x${window.screen.height}`,
+                language: navigator.language,
+                platform: navigator.platform
+              }
+            },
+            stream: false
+          })
+                });
       console.log('Respuesta recibida del backend:', response);
       return response.ok;
     } catch (error) {
