599138cd8150fcdae89b144ffac6bf8e24ae2874
AgustinMadygraf <agustin.mtto.madygraf>
2024-12-31 00:58:35 -0300

    Enhance MermaidDiagram.vue with additional explanatory text for diagram elements and improve readability of the Mermaid graph structure

--- src/components/MermaidDiagram.vue
+++ src/components/MermaidDiagram.vue
@@ -5,13 +5,18 @@ Este componente renderiza un diagrama Mermaid en la aplicación con mejor manejo
 
 <template>
     <div>
+        <p>
+      Las clases marcadas con <i>&lt;I&gt;</i> son interfaces; las marcadas con <i>&lt;DS&gt;</i> son Estructuras de Datos.
+      Las flechas de cabeza abierta son relaciones en uso.
+      Las flechas de cabeza cerrada son relaciones de <i>implementación</i> o de <i>herencia</i>.
+    </p>
       <div id="mermaid">
         graph TD;
-    App[App.vue] --> NetworkCheck[NetworkCheck.vue];
-    App --> NavBar[NavBar.vue];
-    App --> MenuTabs[MenuTabs.vue];
-    MenuTabs --> MadyBot[MadyBot_Vue.vue];
-    MenuTabs --> MermaidDiagram[MermaidDiagram.vue];
+            App[App.vue] --> NetworkCheck[NetworkCheck.vue];
+            App --> NavBar[NavBar.vue];
+            App --> MenuTabs[MenuTabs.vue];
+            MenuTabs --> MadyBot[MadyBot_Vue.vue];
+            MenuTabs --> MermaidDiagram[MermaidDiagram.vue];
     </div>
     </div>
   </template>
