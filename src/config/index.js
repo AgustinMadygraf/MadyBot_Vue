9b8e6586e2dba8996281b9c3b3524179c4d14862
AgustinMadygraf <agustin.mtto.madygraf>
2025-01-03 11:07:45 -0300

    Update config-example.json and index.js to include PHP_ENDPOINT for environment configuration

--- src/config/index.js
+++ src/config/index.js
@@ -11,6 +11,7 @@ const finalConfig = {
   ...config,
   API_ENDPOINT: isProd ? config.API_ENDPOINT_PROD : config.API_ENDPOINT_DEV,
   BASE_URL: isProd ? config.BASE_URL_PROD : config.BASE_URL_DEV,
+  PHP_ENDPOINT: isProd ? config.PHP_ENDPOINT_PROD : config.PHP_ENDPOINT_DEV,
 };
 
 // Validar que finalConfig implementa todas las propiedades de IAppConfig
